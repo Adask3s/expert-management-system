1cd769f51b5494e6d6129c958285bd3a82900908
Jonatan Alimowski <jonatanalimowski@gmail.com>
2026-07-21 14:28:40 +0200

    Create spring boot project with necessary dependencies

---
+++ backend/src/main/java/de/cronn/expertmanagementsystem/ExpertManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package de.cronn.expertmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertManagementSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/cronn/expertmanagementsystem/ExpertManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cronn.expertmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
