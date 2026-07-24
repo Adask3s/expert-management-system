d5a06c7a63603e37b5382b47c8322a3cd768679b
Adam Kopystecki <adam.kopystecki@gmail.com>
2026-07-24 13:24:06 +0200

    Clean up vite boilerplate and setup global css reset

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import App from './App.tsx'
-import './index.css'
+
+// Globalny import tokenów - deklaruje zmienne :root w całej aplikacji
+import './styles/tokens.css';
 
 // tworzymy instancję QueryClient do zarządzania zapytaniami REST API
 const queryClient = new QueryClient({
@@ -21,7 +23,7 @@ createRoot(document.getElementById('root')!).render(
         <QueryClientProvider client={queryClient}>
             {/* dostarczamy mechanizm routingu (ścieżek) do całej aplikacji */}
             <BrowserRouter>
-                <App />
+                <App/>
             </BrowserRouter>
         </QueryClientProvider>
     </StrictMode>,
