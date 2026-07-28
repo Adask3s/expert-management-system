ce8736941312e34b30c747441bd656b4ec19f655
Adam Kopystecki <adam.kopystecki@gmail.com>
2026-07-28 14:47:15 +0200

    Implement Button component with its styles
    Add visualization on DashboardPage

---
+++ frontend/src/components/common/Button/Button.tsx
@@ -0,0 +1,27 @@
+import type {ButtonHTMLAttributes, ReactNode} from "react";
+import styles from './Button.module.css';
+
+// ghost np. dla "Clear All" z Dashboard
+export type ButtonVariant = 'primary' | 'ghost' | 'danger';
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: ButtonVariant;
+    icon?: ReactNode;   // opcjonalna ikona np. "+" dla "+ Add Employee", ReactNode dla uniwersalności (to odpowiada za wszystko,
+                        // co React potrafi wyrenderować
+    children: ReactNode; // children jako tekst przycisku
+}
+
+// ...props - przekazujemy wszystkie pozostałe atrybuty HTML dla przycisku (takie jak onClick)
+// do jednego obiektu nazwanego props, potem je przekazujemy używając <button {...props}>
+export const Button = ({
+                           children, variant = 'primary', icon, className = '', type = 'button', ...props
+                       }: ButtonProps) => {
+    // łączymy klasę bazową z klasą wariantu
+    const combinedClassName = `${styles.base} ${styles[variant]} ${className}`.trim();
+    return (
+        <button type={type} className={combinedClassName} {...props}>
+            {icon && <span className={styles.iconWrapper}>{icon}</span>}
+            {children}
+        </button>
+    )
+}
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -2,7 +2,9 @@ import {UserAvatar} from "../components/common/UserAvatar/UserAvatar.tsx";
 import {Badge} from "../components/common/Badge/Badge.tsx";
 import {ActionIconButton} from "../components/common/IconButton/ActionIconButton.tsx";
 import editIcon from '../assets/icons/Edit.svg'; // impotrujemy ikonę edytowania
-import deleteIcon from '../assets/icons/Delete.svg'; // impotrujemy ikonę usuwania
+import deleteIcon from '../assets/icons/Delete.svg';
+import plusIcon from '../assets/icons/Plus.svg';
+import {Button} from "../components/common/Button/Button.tsx"; // impotrujemy ikonę usuwania
 
 export const DashboardPage = () => {
     return (
@@ -44,6 +46,14 @@ export const DashboardPage = () => {
                     variant="danger"
                     onClick={() => console.log('Delete action.')}
                 />
+            </div>
+            <br/>
+            <div style={{display: 'flex', gap: '12px', alignItems: 'center'}}>
+                <Button variant="primary" icon={<img src={plusIcon} alt="" aria-hidden="true"/>}
+                        onClick={() => console.log('Primary Action')}>Add
+                    Employee</Button>
+                <Button variant="danger" onClick={() => console.log('Danger Action')}>Restore Default</Button>
+                <Button variant="ghost" onClick={() => console.log('Ghost Action')}>Clear All</Button>
             </div>
         </div>
     );
