0db0ef262a7a49351126962479a2b9c5e7b32122
Jonatan Alimowski <jonatanalimowski@gmail.com>
2026-07-30 13:16:38 +0200

    Add temporary configuration to allow all requests from allowed origins

--- backend/src/main/java/de/cronn/expertmanagementsystem/config/SecurityConfig.java
+++ backend/src/main/java/de/cronn/expertmanagementsystem/config/SecurityConfig.java
@@ -1,19 +1,29 @@
 package de.cronn.expertmanagementsystem.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 // 1. Wyłączamy CSRF, ponieważ budujemy bezstanowe REST API
                 .csrf(AbstractHttpConfigurer::disable)
 
@@ -23,5 +33,21 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    // temporarily we allow all requests from allowedOrigins
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(allowedOrigins));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
