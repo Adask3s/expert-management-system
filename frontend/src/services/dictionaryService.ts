ee9b2ee985b746b43dc7d7941bede487a351a307
Adam Kopystecki <adam.kopystecki@gmail.com>
2026-08-06 13:56:22 +0200

    Fix merge conflicts

--- frontend/src/services/dictionaryService.ts
+++ frontend/src/services/dictionaryService.ts
@@ -4,8 +4,8 @@
 // Widok zarządzania domenami również itp.
 
 import {axiosClient} from '../api/axiosClient';
-import type {Domain, DomainPageDto, DomainRequest, ExpertiseLevel} from '../types/users';
 import type {DomainRequestFormData} from '../validations/domainSchema';
+import type {Domain, DomainPageDto, DomainRequest, ExpertiseLevel} from '../types/users';
 
 // Przygotowujemy serwis na przyszły task o wyszukiwaniu domen.
 // Dlatego metoda getDomains przyjmuje dodatkowy parametr name, który jest opcjonalny.
